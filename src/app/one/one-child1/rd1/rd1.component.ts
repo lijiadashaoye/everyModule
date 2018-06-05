b95364d43a85ca766b3d486f9b75b8c4f88ce028
lipeng <lipengd@litsoft.com.cn>
2018-06-05 22:25:24 +0800

    合并

---
+++ src/app/one/one-child1/rd1/rd1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rd1',
+  templateUrl: './rd1.component.html',
+  styleUrls: ['./rd1.component.css']
+})
+export class Rd1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
