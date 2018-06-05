fa6eaa73e931a48218f2f5b109732a05117a9ee4
lipeng <lipengd@litsoft.com.cn>
2018-06-05 22:28:04 +0800

    合并

---
+++ src/app/popup/popup.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-popup',
+  templateUrl: './popup.component.html',
+  styleUrls: ['./popup.component.css']
+})
+export class PopupComponent implements OnInit {
+  path;
+  constructor(
+    private router: Router,
+    private act: ActivatedRoute
+  ) { }
+  ngOnInit() {
+    this.act.data.subscribe(val => this.path = val.data)
+  }
+  close() {
+      switch (this.path) {
+        case '1': this.router.navigate([{ outlets: { popup1: null } }]);
+          break;
+        case '2': this.router.navigate([{ outlets: { popup2: null } }]);
+          break;
+        default: this.router.navigate([{ outlets: { popup3: null } }]);
+      }
+  }
+
+}
