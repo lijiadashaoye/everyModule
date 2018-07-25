bad5d4b8b0a3d63613463556df4cd080482601f6
lipeng <937681195@qq.com>
2018-07-25 17:56:58 +0800

    f

--- src/app/one/one-child1/one-child1.component.ts
+++ src/app/one/one-child1/one-child1.component.ts
@@ -153,7 +153,7 @@ export class OneChild1Component implements OnInit {
   es6() {
     // ES7中代替indexOf方法，用于查找数组元素
     let arr = ['react', 'angular', 'vue'];
-    console.log(arr.includes('react'));
+    console.log(arr.includes('react'));     // true
 
     // ES7中求幂运算
     let bb = 2 ** 2;
@@ -161,7 +161,7 @@ export class OneChild1Component implements OnInit {
 
     // 你使用老方式for /in (ES5)也许用的非常好。但是他会迭代所有可以枚举属性（像原型中的带名字的）, 
     // 不仅仅自己的属性，会意外的破坏那些 像prototype和tostring得到意想不到的值。
-    // Object.values返回对象自身可以迭代属性值（values）为数组类型。
+    // Object.values返回对象自身可以迭代的属性的值，返回值组成的数组。
     // 我们最好使用Array.prototype.forEach迭代它，结合ES6的箭头函数隐形返回值：
 
     let obj = { a: 1, b: 2, c: 3 }
@@ -179,6 +179,62 @@ export class OneChild1Component implements OnInit {
     // Math.trunc方法用于去除一个数的小数部分，返回整数部分。
     let one = Math.trunc(4.231);
     console.log(one)
+
+    let arr1 = [1, 2, 3, 4, 5, 6, 7];
+    console.log(arr1.filter(x => x > 3))  // 返回一个符合条件的数组
+    console.log(arr1.find(x => x > 3))  // 返回第一个符合条件的数组元素
+
+    // 比较任意两个值是否完全相等
+    console.log(Object.is('foo', 'foo'))  // true
+    console.log(Object.is(+0, -0))        // false
+    console.log(Object.is(NaN, NaN))      // true
+
+    // Object.keys()：返回对象自身的所有可枚举的属性的键名（不含继承的）。
+
+    // 用Set函数进行数组去重
+    let arr2 = [1, 2, 2, 3, 4, 4];
+    let set = new Set(arr2);  // 新建Set函数，
+    set.add(10);
+    console.log(set.size);
+    console.log([...Array.from(set)]);
+    set.delete(2);
+    console.log([...Array.from(set)]);
+
+    let a = new Set([1, 2, 3]);
+    let b = new Set([4, 3, 2]);
+
+    // 并集
+    let union = new Set([...Array.from(a), ...Array.from(b)]); // Set {1, 2, 3, 4}
+    // 交集
+    let intersect = new Set([...Array.from(a)].filter(x => b.has(x))); // set {2, 3}
+    // 差集
+    let difference = new Set([...Array.from(a)].filter(x => !b.has(x)));  // Set {1}
+
+    console.log(union)
+    console.log(intersect)
+    console.log(difference)
+
+    // 使用proxy 对对象进行读取拦截
+    let lll = {
+      // 参数target是目标对象，参数prop是属性名。返回值如果含有该属性，返回true，否则返回false
+      set: function (obj, prop, value) {
+        obj[prop] = value;
+        return true;
+      },
+      // get方法用于拦截某个属性的读取操作，可以接受三个参数，依次为目标对象、属性名
+      // 和 proxy 实例本身（严格地说，是操作行为所针对的对象），其中最后一个参数可选。
+      get: function (target, property) {
+        if (property in target) {
+          return target[property] + '*******'
+        } else {
+          return 35;
+        }
+      },
+    }
+    let proxy = new Proxy({}, lll);
+    console.log(proxy['time']) // 35
+    proxy['time'] = 99;
+    console.log(proxy['time'])
   }
 
   findDoms; // 用来保存查找到的元素
@@ -191,5 +247,6 @@ export class OneChild1Component implements OnInit {
     this.tickDom = kk.filter(dom => dom['id'] === `ticked${id}`);
     this.rd.setStyle(this.tickDom[0], 'background', 'red')
   }
+
 }
 
