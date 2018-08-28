bec501ef5854de908bf791a1b8cba92c9b1f11e9
lipeng <937681195@qq.com>
2018-08-28 16:21:20 +0800

    f

--- src/app/three/three-child1/three-child1.component.ts
+++ src/app/three/three-child1/three-child1.component.ts
@@ -101,6 +101,7 @@ export class ThreeChild1Component implements OnInit {
   // async函数返回一个 Promise 对象，可以使用then方法添加回调函数。
   // 当函数执行的时候，一旦遇到await就会先返回，等到异步操作完成，再接着执行函数体内后面的语句。
   // await命令后面，可以是 Promise 对象和原始类型的值（数值、字符串和布尔值，但这时等同于同步操作）。
+  // 正常情况下，await命令后面是一个 Promise 对象。如果不是，会被转成一个立即resolve的 Promise 对象。
   testasync() {
     function timeout(ms) {
       return new Promise(resolve => {
